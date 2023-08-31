ec3849b5413a6e64a3d73ab85ea2bf15a1490524
Pedro Carrasco <pacarrasco@fonasa.gov.cl>
2023-08-31 16:26:40 -0400

    ajuste

--- ejecutar_contenedor.sh
+++ ejecutar_contenedor.sh
@@ -1,4 +1,5 @@
-export DB_PATH=/opt/base_datos.db
-export FLASK_APP=app
+export ARCHIVO_SQLITE=/opt/base_datos.db
+export FLASK_APP=api-pass:app
 export FLASK_ENV=development
+python api-pass/crear_tabla.py
 flask run --host=0.0.0.0
